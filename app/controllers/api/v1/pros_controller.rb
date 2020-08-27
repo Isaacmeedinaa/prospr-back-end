4c9c421e4576f67c2d6c9cb3844ead3eee48676e
Isaac Medina <isaac.meedinaa@gmail.com>
2020-08-27 15:28:16 -0500

    finished Pro register and validations

--- app/controllers/api/v1/pros_controller.rb
+++ app/controllers/api/v1/pros_controller.rb
@@ -1,4 +1,9 @@
 class Api::V1::ProsController < ApplicationController
+
+    skip_action_before :authorized, [:register]
+
+    def register
+    end
     
     def index
         @pros = Pro.all
@@ -8,6 +13,15 @@ class Api::V1::ProsController < ApplicationController
     def show
         @pro = Pro.find_by(id: params[:id])
         render json: @pro
+    end
+
+    private
+
+    def pro_register_params
+        params.require(:browser).permit(:first_name, :last_name, :email, :phone_number, :password, :confirm_password)
+    end
+
+    def pro_update_params
     end
 
 end
--- app/models/pro.rb
+++ app/models/pro.rb
@@ -3,5 +3,26 @@ class Pro < ApplicationRecord
     has_many :prosp_likes, through: :prosps
     has_one :payment_method, dependent: :destroy
 
+    # validations
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, presence: true, uniqueness: true, case_sensitive: false, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :phone_number, presence: true, uniqueness: true, format: { with: /\A\d+\z/ }
+    validates :password, presence: true, length: { minimum: 6 }, confirmation: true
+    validate :password_requirements
+
+    # password protection
     has_secure_password
+
+    def password_requirements
+        rules = {
+            "must contain at least one uppercase letter"  => /[A-Z]+/,
+            "must contain at least one digit"             => /\d+/,
+            "must contain at least one special character" => /[^A-Za-z0-9]+/
+        }
+
+        rules.each do |message, regex|
+            errors.add(:password, message) unless password.match(regex)
+        end
+    end
 end
