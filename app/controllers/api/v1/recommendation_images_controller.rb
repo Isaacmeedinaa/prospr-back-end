959ec492cbb35983ee8ef64c9d6f323d71adfa3a
Isaac Medina <isaac.meedinaa@gmail.com>
2020-09-10 21:52:10 -0500

    added recommendation image create method

--- app/controllers/api/v1/recommendation_images_controller.rb
+++ app/controllers/api/v1/recommendation_images_controller.rb
@@ -10,4 +10,20 @@ class Api::V1::RecommendationImagesController < ApplicationController
         render json: @recommendation_image
     end
 
+    def create
+        @recommendation_image = RecommendationImage.create(recommendation_image_params)
+
+        if @recommendation_image.valid?
+            render json: { status: 200, recommendation_image: RecommendationImageSerializer.new(@recommendation_image) }
+        else
+            render json: { status: 401, error_messages: @recommendation_image.errors.full_messages }
+        end
+    end
+
+    private
+
+    def recommendation_image_params
+        params.require(:recommendation_image).permit(:img_url, :recommendation_id)
+    end
+
 end
