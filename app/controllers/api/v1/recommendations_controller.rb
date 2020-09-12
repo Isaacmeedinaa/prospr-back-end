b2b6f06314c9444433c15d073d3b5153b3996dfc
Isaac Medina <isaac.meedinaa@gmail.com>
2020-09-12 16:17:17 -0500

    added temp solution to recommendations feed

--- app/controllers/api/v1/recommendations_controller.rb
+++ app/controllers/api/v1/recommendations_controller.rb
@@ -3,19 +3,31 @@ class Api::V1::RecommendationsController < ApplicationController
     skip_before_action :authorized, only: [:index, :show]
 
     def index
-        @recommendations = Recommendation.all.order('created_at DESC').paginate(page: params[:page], per_page: 4)
+        # @recommendations = Recommendation.all.order('created_at DESC').paginate(page: params[:page], per_page: 4)
+        @recommendations = Recommendation.all.order('created_at DESC')
         render json: @recommendations
     end
 
     def show
         @recommendation = Recommendation.find_by(id: params[:id])
-        render json: @recommendations
+        if @recommendation
+            render json: { status: 200, recommendation: RecommendationSerializer.new(@recommendation) }
+        else
+            render json: { status: 201, error_messages: @recommendation.errors.full_messages }
+        end
     end
 
     def create
-        @recommendation = Recommendation.create(recommendation_params)
+        @recommendation = Recommendation.new(title: params[:title], content: params[:content], browser_id: params[:browser_id])
 
         if @recommendation.valid?
+
+            @recommendation.save
+
+            params[:media_urls].each do |media_url|
+                RecommendationImage.create(img_url: media_url, recommendation_id: @recommendation.id)
+            end
+
             render json: { status: 200, recommendation: RecommendationSerializer.new(@recommendation) }
         else
             render json: { status: 401, error_messages: @recommendation.errors.full_messages }
@@ -44,9 +56,4 @@ class Api::V1::RecommendationsController < ApplicationController
         end
     end
 
-    private 
-
-    def recommendation_params
-        params.require(:recommendation).permit(:title, :content, :browser_id)
-    end
 end
